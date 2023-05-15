4546bc99b372b8f9098be5936a52dc6ad4f00add
meritozh <meritozh@users.noreply.github.com>
2023-05-15 12:21:38 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="meritozh"
+original_name="richesse"
+original_urlname="richesse"
+original_description="Awesome richesse created by meritozh"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv richesse $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
